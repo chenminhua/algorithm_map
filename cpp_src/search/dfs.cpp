e096400d5111019329c809438614f11dd4628f27
chenminhua <chenmh@blacklake.cn>
2018-07-02 20:23:44 +0800

    stack

--- cpp_src/search/dfs.cpp
+++ cpp_src/search/dfs.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
+#include <stack>
 #define MAX_ROW 5
 #define MAX_COL 5
-struct point
+typedef struct point
 {
   int row, col;
-} stack[512];
-int top = 0;
-void push(struct point p)
-{
-  stack[top] = p;
-  top++;
-}
-struct point pop(void)
-{
-  top--;
-  return stack[top];
-}
-int is_empty(void)
-{
-  return top == 0;
-}
+} point;
 int maze[MAX_ROW][MAX_COL] = {
     {0, 1, 0, 0, 0},
     {0, 1, 0, 1, 0},
     {0, 0, 0, 0, 0},
     {0, 1, 1, 1, 0},
     {0, 0, 0, 1, 0}};
+
+std::stack<point> st;
 
 void print_maze(void)
 {
@@ -53,7 +41,7 @@ void visit(int row, int col, struct point pre)
   struct point visit_point = {row, col};
   maze[row][col] = 2;
   predecessor[row][col] = pre;
-  push(visit_point);
+  st.push(visit_point);
 }
 
 // 将起点标记为已走过并压栈;
@@ -70,11 +58,12 @@ int main(void)
 {
   struct point p = {0, 0};
   maze[p.row][p.col] = 2;
-  push(p);
+  st.push(p);
 
-  while (!is_empty())
+  while (!st.empty())
   {
-    p = pop();
+    p = st.top();
+    st.pop();
     if (p.row == MAX_ROW - 1 /* goal */ && p.col == MAX_COL - 1)
       break;
     if (p.col + 1 < MAX_COL /* right */
